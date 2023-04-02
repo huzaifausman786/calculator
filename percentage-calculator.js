3609bee7b8e7e00fbe6152169558a7edf689e52c
Muhammad Huzaifa <huzaifausman2k2@gmail.com>
2023-04-02 03:30:28 -0700

    Calculator

---
+++ percentage-calculator.js
@@ -0,0 +1,9 @@
+var course1 = Number(prompt("enter your marks"));
+var course2 = Number(prompt("enter your marks"));
+var course3 = Number(prompt("enter your marks"));
+var course4 = Number(prompt("enter your marks"));
+var course5 = Number(prompt("enter your marks"));
+var totalmarks = 500;
+obtainedmarks = course1+course2+course3+course4+course5;
+var result = obtainedmarks * 100/ totalmarks
+alert(result);
